ed9e4d862466f281e94067e676165638d41cb6d9
thecloer <chltjrrb97@gmail.com>
2025-09-20 21:51:04 +0900

    [feat] field에 aria-label과 placeholder를 추가했습니다.

--- src/components/application-form-fields.tsx
+++ src/components/application-form-fields.tsx
@@ -10,11 +10,29 @@ const careerOptions = [
 
 export const ApplicationFormFields: FC = () => (
   <>
-    <Input label='이름 / 닉네임' name='name' type='text' required autoComplete='name' />
+    <Input
+      label='이름 / 닉네임'
+      aria-label='닉네임'
+      name='name'
+      type='text'
+      required
+      autoComplete='name'
+    />
 
-    <Input label='이메일' name='email' type='email' required autoComplete='email' />
+    <Input
+      label='이메일'
+      aria-label='이메일'
+      name='email'
+      type='email'
+      required
+      autoComplete='email'
+    />
 
-    <Select label='FE 경력 연차' name='career' aria-label='career'>
+    <Select label='FE 경력 연차' aria-label='FE 경력 연차' name='career'>
+      <option value='' disabled selected hidden>
+        선택해주세요
+      </option>
+
       {careerOptions.map(({ value, label }) => (
         <option key={value} value={value} aria-label={label}>
           {label}
@@ -22,6 +40,13 @@ export const ApplicationFormFields: FC = () => (
       ))}
     </Select>
 
-    <Input label='GitHub 링크 (선택)' name='github' type='url' autoComplete='email' />
+    <Input
+      label='GitHub 링크 (선택)'
+      aria-label='github 링크'
+      name='github'
+      type='url'
+      autoComplete='email'
+      placeholder='https//:github.com/username'
+    />
   </>
 );
