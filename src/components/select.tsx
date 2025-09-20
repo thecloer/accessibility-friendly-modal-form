edf3cdcf890160a71cf2fea706ff596e175570c9
thecloer <chltjrrb97@gmail.com>
2025-09-20 21:30:25 +0900

    [fix] mordern-normalize로 인해 tailwindcss가 적용되지 않던 버그를 수정했습니다.
    - tailwindcss는 자체적으로 기본 스타일을 지우므로 mordern-normalize 의존성을 제거했습니다.

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -3,18 +3,18 @@ import { useId, type FC, type SelectHTMLAttributes } from 'react';
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
 }
-export const Select: FC<SelectProps> = ({ id: customId, label, className, ...props }) => {
+export const Select: FC<SelectProps> = ({ id: customId, label, className = '', ...props }) => {
   const generatedId = useId();
   const id = customId ?? generatedId;
 
   return (
-    <div className={`flex flex-col w-full gap-2 ${className}`}>
+    <div className={`field-wrapper ${className}`}>
       {label == null ? null : (
-        <label htmlFor={id} className='font-semibold'>
+        <label htmlFor={id} className='label'>
           {label}
         </label>
       )}
-      <select id={id} className='px-4 py-3 border border-neutral-200 rounded' {...props} />
+      <select id={id} className='field' {...props} />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import 'modern-normalize';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import ModalFormPage from './ModalFormPage';
 import './global.css';
+import ModalFormPage from './ModalFormPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
